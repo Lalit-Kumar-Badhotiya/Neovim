68566f8561ff0b8e2639de428d65451a076eddbc
LalitKumar0day <lalitkumar.ac105@gmail.com>
2024-08-31 18:10:20 +0530

    added my nvim config

---
+++ nvim/init.lua
@@ -0,0 +1,26 @@
+require("config.lazy")
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.g.mapleader = " "
+
+
+
+
+
+--~/.config(Local in windows)/nvim
+--├── lua
+--│   ├── config
+--│   │   └── lazy.lua
+--│   └── plugins
+--│       ├── spec1.lua
+--│       ├── **
+--│       └── spec2.lua
+--└── init.lua
+
+
+-- if you are seeing this 
+-- hiii from me (lalit)
+--  STFU im not a nerd,
+--  and hey!! THANKS NERD.
---
+++ readme.md
@@ -0,0 +1,10 @@
+##File Structure
+~/.config(Local in windows)/nvim
+├── lua
+│ ├── config
+│ │ └── lazy.lua
+│ └── plugins
+│ ├── spec1.lua
+│ ├── \*\*
+│ └── spec2.lua
+└── init.lua
